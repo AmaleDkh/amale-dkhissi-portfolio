a1c7b06780dc85e590d55d34e8c56458562c968c
Amale Dkhissi <amdkhissi@gmail.com>
2024-07-10 23:52:25 +0200

    refactor(modal): change style

--- src/components/ProjectModal/ProjectModal.jsx
+++ src/components/ProjectModal/ProjectModal.jsx
@@ -1,5 +1,5 @@
 // React element
-import Modal from "react-modal";
+import { useEffect } from "react";
 
 // Style
 import "./ProjectModal.scss";
@@ -7,23 +7,6 @@ import "./ProjectModal.scss";
 // Font Awesome icon
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-
-const customModalStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-    width: "50%",
-    height: "50%",
-    border: "none",
-    padding: "0 0 20px 0",
-  },
-};
-
-Modal.setAppElement("#root");
 
 function ProjectModal({
   modalIsOpen,
@@ -35,55 +18,71 @@ function ProjectModal({
   source,
   image,
 }) {
+  useEffect(() => {
+    if (modalIsOpen) {
+      document.body.classList.add("body__modal-open");
+    } else {
+      document.body.classList.remove("body__modal-open");
+    }
+
+    return () => {
+      document.body.classList.remove("body__modal-open");
+    };
+  }, [modalIsOpen]);
+
   const openSourceLink = () => {
     window.open(source, "_blank");
   };
 
+  const handleBackgroundClick = (e) => {
+    if (e.target.classList.contains("modal__background")) {
+      closeModal();
+    }
+  };
+
   return (
-    <Modal
-      isOpen={modalIsOpen}
-      onRequestClose={closeModal}
-      style={customModalStyles}
-      contentLabel="Modale de projet"
-    >
-      <img src={image} alt="" />
-      <div className="modal__project-container__information">
-        <h2 className="modal__project-container__information__title">
-          {title}
-        </h2>
-        <div className="modal__project-container__information__description">
-          {description}
-        </div>
-        <div className="modal__project-container__information__details">
-          {details}
-        </div>
-        <div className="modal__project-container__information__technologies">
-          <div className="modal__project-container__information__technologies__title">
-            Technologies utilisées :
+    <>
+      {modalIsOpen && (
+        <div className="modal__background" onClick={handleBackgroundClick}>
+          <div className="modal__content">
+            <span className="modal__close" onClick={closeModal}>
+              &times;
+            </span>
+            <img src={image} alt="" className="modal__image" />
+            <div className="modal__information">
+              <h2 className="modal__title">{title}</h2>
+              <div className="modal__description">{description}</div>
+              <div className="modal__details">{details}</div>
+              <div className="modal__technologies">
+                <div className="modal__technologies__title">
+                  Technologies utilisées :
+                </div>
+                <div className="modal__technologies__list">
+                  {technologies
+                    ? technologies.map((tech, techIndex) => (
+                        <i
+                          key={techIndex}
+                          className={`${tech.icon} ${tech.size}`}
+                          title={tech.technology}
+                        ></i>
+                      ))
+                    : null}
+                </div>
+              </div>
+              <div className="modal__link">
+                <button
+                  className="modal__link__project"
+                  onClick={openSourceLink}
+                >
+                  <FontAwesomeIcon icon={faArrowRight} />
+                  Découvrir le projet sur GitHub
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="modal__project-container__information__technologies__technos">
-            {technologies
-              ? technologies.map((tech, techIndex) => (
-                  <i
-                    key={techIndex}
-                    className={`${tech.icon} ${tech.size}`}
-                    title={tech.technology}
-                  ></i>
-                ))
-              : null}
-          </div>
         </div>
-        <div className="modal__project-container__information__link">
-          <button
-            onClick={openSourceLink}
-            className="modal__project-container__information__link__project"
-          >
-            <FontAwesomeIcon icon={faArrowRight} />
-            Découvrir le projet sur GitHub
-          </button>
-        </div>
-      </div>
-    </Modal>
+      )}
+    </>
   );
 }
 
