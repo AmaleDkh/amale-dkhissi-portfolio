357ca2533139e35f86441b54eecd549c23923ca9
Amale Dkhissi <amdkhissi@gmail.com>
2024-07-10 23:46:28 +0200

    refactor(experiences): change information in array

--- src/components/ExperienceItem/ExperienceItem.jsx
+++ src/components/ExperienceItem/ExperienceItem.jsx
@@ -5,16 +5,12 @@ function ExperienceItem({ year, description, experience }) {
   return (
     <div className="experience-item">
       <div className="experience-item__year">{year}</div>
-      {description ? (
-        <div>{description}</div>
-      ) : (
-        <div className="experience-item__information">
-          <div className="experience-item__information__company">
-            {experience.company}
-          </div>
-          <div>{experience.position}</div>
+      <div className="experience-item__information">
+        <div className="experience-item__information__company">
+          {experience.company}
         </div>
-      )}
+        <div>{experience.position}</div>
+      </div>
     </div>
   );
 }
