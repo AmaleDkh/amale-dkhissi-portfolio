e8aa65b58c73e82f9a88381e257ab6d35d9e5927
Amale Dkhissi <amdkhissi@gmail.com>
2024-07-11 20:59:13 +0200

    feature(header): add hamburger m enu for responsive versions

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,6 @@
+// React element
+import { useState } from "react";
+
 // Style
 import "./Header.scss";
 
@@ -9,6 +12,12 @@ function Header() {
     }
   };
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="header">
       <div className="header__title">Amale Dkhissi</div>
@@ -18,6 +27,57 @@ function Header() {
         <div onClick={() => scrollToSection("skills")}>Compétences</div>
         <div onClick={() => scrollToSection("contact")}>Contact</div>
       </nav>
+
+      <div className="menu-hamburger">
+        <button
+          className={`menu-hamburger__button ${isOpen ? "open" : ""}`}
+          onClick={toggleMenu}
+          aria-expanded={isOpen}
+          aria-label="Toggle navigation menu"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        {isOpen && (
+          <nav className={`menu-hamburger__nav ${isOpen ? "open" : ""}`}>
+            <ul>
+              <li
+                onClick={() => {
+                  scrollToSection("about");
+                  setIsOpen(false);
+                }}
+              >
+                À propos
+              </li>
+              <li
+                onClick={() => {
+                  scrollToSection("projects");
+                  setIsOpen(false);
+                }}
+              >
+                Projets
+              </li>
+              <li
+                onClick={() => {
+                  scrollToSection("skills");
+                  setIsOpen(false);
+                }}
+              >
+                Compétences
+              </li>
+              <li
+                onClick={() => {
+                  scrollToSection("contact");
+                  setIsOpen(false);
+                }}
+              >
+                Contact
+              </li>
+            </ul>
+          </nav>
+        )}
+      </div>
     </header>
   );
 }
