3056e475fa00a36189c0d0fbbb428a0b01b91ed4
Amale Dkhissi <amdkhissi@gmail.com>
2024-07-11 21:30:54 +0200

    refactor(contact): change style

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,88 +1,92 @@
 // React element
-import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 // Component
 import SectionTitle from "../SectionTitle/SectionTitle";
 
 // Style
-import "./Contact.scss";
+import "./../Contact/Contact.scss";
 
 function Contact() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    message: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
 
   return (
     <div id="contact" className="contact">
       <SectionTitle title="Contact" />
-      <form onSubmit={handleSubmit(onSubmit)} className="contact__form">
+      <form onSubmit={handleSubmit} className="contact__form">
         <div className="contact__form__container">
           <label htmlFor="firstName">Prénom :</label>
           <input
             id="firstName"
+            name="firstName"
+            type="text"
             className="contact__form__container__input-information"
-            {...register("firstName", {
-              required: "Le prénom est requis",
-              pattern: {
-                value: /^[A-Za-z]+$/i,
-                message: "Le prénom doit contenir uniquement des lettres",
-              },
-            })}
+            value={formData.firstName}
+            onChange={handleInputChange}
+            placeholder="Entrez votre prénom"
+            required
           />
-          {errors.firstName && <p>{errors.firstName.message}</p>}
         </div>
         <div className="contact__form__container">
           <label htmlFor="lastName">Nom :</label>
           <input
             id="lastName"
+            name="lastName"
+            type="text"
             className="contact__form__container__input-information"
-            {...register("lastName", {
-              required: "Le nom est requis",
-              pattern: {
-                value: /^[A-Za-z]+$/i,
-                message: "Le nom doit contenir uniquement des lettres",
-              },
-            })}
+            value={formData.lastName}
+            onChange={handleInputChange}
+            placeholder="Entrez votre nom"
+            required
           />
-          {errors.lastName && <p>{errors.lastName.message}</p>}
         </div>
         <div className="contact__form__container">
           <label htmlFor="email">Adresse e-mail :</label>
           <input
             id="email"
+            name="email"
             type="email"
             className="contact__form__container__input-information"
-            {...register("email", {
-              required: "L'adresse e-mail est requise",
-              pattern: {
-                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-                message: "Adresse e-mail invalide",
-              },
-            })}
+            value={formData.email}
+            onChange={handleInputChange}
+            placeholder="Entrez votre adresse e-mail"
+            required
           />
-          {errors.email && <p>{errors.email.message}</p>}
         </div>
         <div className="contact__form__container">
           <label htmlFor="message">Message :</label>
           <textarea
             id="message"
+            name="message"
             className="contact__form__container__input-message"
-            {...register("message", {
-              required: "Le message est requis",
-              maxLength: {
-                value: 500,
-                message: "Le message ne doit pas dépasser 500 caractères",
-              },
-            })}
+            value={formData.message}
+            onChange={handleInputChange}
+            placeholder="Entrez votre message"
+            required
           />
-          {errors.message && <p>{errors.message.message}</p>}
         </div>
         <input
           type="submit"
           className="contact__form__container__input-submit"
+          value="Envoyer"
         />
       </form>
     </div>
